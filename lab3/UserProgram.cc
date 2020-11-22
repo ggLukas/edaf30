f4b0dd3174b1cf16aea3129cc0481ea6e24566b0
Emil <ungothar97@gmail.com>
2020-11-22 15:17:42 +0100

    finished lab3 wawa we man

--- lab3/UserProgram.cc
+++ lab3/UserProgram.cc
@@ -1,7 +1,22 @@
 #include <iostream>
+#include "libs/UserTable.h"
+
+using std::endl;
+using std::cout;
 
 int main(){
-    std::cout << "HELLO!" << std::endl;
+    UserTable table{"users.txt"};
+    
+    cout << table.find(21330) << endl;
+    cout << table.find(21331) << endl;
+    cout << table.find("Jens Holmgren") << endl; 
+
+    table.addUser(User{1234, "Jon Von Bonvi"});
+    table.addUser(User{1235, "Golgoth, Shadow of Nightmares and The Voidkiller of Slayer Destruction, Smiter of Heavens"});
+
+
+    cout << table.find(1235) << endl; 
+    cout << testFindNbr(table) << endl;
 
     return 0;
 }
--- lab3/libs/UserTable.cc
+++ lab3/libs/UserTable.cc
@@ -2,47 +2,56 @@
 #include <fstream>
 #include <algorithm>
 
-const User UserTable::user_not_found = User{-1,"Not Found"};
+const User UserTable::user_not_found = User{-1, "Not Found"};
 
-UserTable::UserTable() :users{new User[capacity]} { }
+UserTable::UserTable() : users{new User[capacity]} {}
 
-UserTable::UserTable(const std::string& fname) :UserTable{}
+UserTable::UserTable(const std::string &fname) : UserTable{}
 {
     std::ifstream ufile(fname);
 
-    if(ufile.is_open()) {
-        while(ufile) {
+    if (ufile.is_open())
+    {
+        while (ufile)
+        {
             int cn;
-            if(ufile >> cn ) {
+            if (ufile >> cn)
+            {
                 ufile.ignore(); // skip space
                 char n[80];
-                ufile.getline(n,80);
-                addUser(User(cn,n));
-
+                ufile.getline(n, 80);
+                addUser(User(cn, n));
             }
         }
-    } else {
+    }
+    else
+    {
         std::cout << "Could not open " << fname << std::endl;
     }
 }
 
-void UserTable::addUser(const User& u)
+void UserTable::addUser(const User &u)
 {
     // gör tabellen större vid behov
-    ensureCapacity(n+1);
+    ensureCapacity(n + 1);
     // 1. Hitta rätt plats
     int pos{0};
-    while ( (pos < n) && (users[pos].getCardNbr() < u.getCardNbr())){
+    while ((pos < n) && (users[pos].getCardNbr() < u.getCardNbr()))
+    {
         ++pos;
     }
 
     //2. skapa lucka i vektorn
-    for(int i=n; i > pos; --i){
-        users[i] = users[i-1];
+    for (int i = n; i > pos; --i)
+    {
+        users[i] = users[i - 1];
     }
 
     //3. stoppa in den nya användaren i luckan
     users[pos] = u;
+
+    //4. Öka n med ett
+    n++;
 }
 
 User UserTable::find(int c) const
@@ -53,28 +62,35 @@ User UserTable::find(int c) const
     int high = n - 1;
     int mid = -1;
     bool found = false;
-    while (low < high && ! found) {
+    while (low <= high && !found)
+    {
         mid = (low + high) / 2;
         //
         int midnbr = users[mid].getCardNbr();
-        if (midnbr = c) {
+        if (midnbr == c)
+        {
             found = true;
-        } else if (users[mid].getCardNbr() < c) {
+        }
+        else if (users[mid].getCardNbr() < c)
+        {
             low = mid + 1;
-        } else {
-            high = mid - 1;
+        }
+        else
+        {
+            high = mid-1;
         }
     }
 
     return found ? users[mid] : user_not_found;
 }
+
 User UserTable::find(std::string name) const
 {
-    for (int i = 0; i != n; ++i) {
-        if (users[i].getName() == name) {
+    for (int i = 0; i != n; ++i)
+    {
+        if (users[i].getName() == name)
+        {
             return users[i];
-        } else {
-            return user_not_found;
         }
     }
     return user_not_found;
@@ -82,41 +98,46 @@ User UserTable::find(std::string name) const
 
 void UserTable::ensureCapacity(int s)
 {
-    if(s>capacity) {
-        while(s > capacity) {
-            capacity*=4;
+    if (s > capacity)
+    {
+        while (s > capacity)
+        {
+            capacity *= 4;
         }
         auto tmp = new User[capacity];
-        std::copy(users, users+n, tmp);
+        std::copy(users, users + n, tmp);
         delete[] users;
-        users=tmp;
+        users = tmp;
     }
-
 }
-void UserTable::print(std::ostream& os) const
+
+void UserTable::print(std::ostream &os) const
 {
     os << "-------------" << std::endl;
-    for(int i = 0; i != getNbrUsers(); ++i) {
-        const auto& u = users[i];
-        os << "(" <<u.getCardNbr() << ") " << u.getName() << std::endl;
+    for (int i = 0; i != getNbrUsers(); ++i)
+    {
+        const auto &u = users[i];
+        os << "(" << u.getCardNbr() << ") " << u.getName() << std::endl;
     }
     os << "=============" << std::endl;
 }
+
 /**
     * Testmetod för binärsökningen:
     * går igenom alla användare och kollar att deras kortnummer kan sökas upp.
     * Om något kortnummer inte kunde sökas upp returneras detta. Annars, om
     * alla sökningar lyckades, returneras 0.
     */
-int testFindNbr(const UserTable ut)
+int testFindNbr(const UserTable &ut)
 {
-    for (int i = 0; i < ut.n; i++) {
+    for (int i = 0; i < ut.n; i++)
+    {
         int nbr = ut.users[i].getCardNbr();
         User found = ut.find(nbr);
-        if (found != ut.users[i]) {
+        if (found != ut.users[i])
+        {
             return nbr;
         }
     }
     return 0;
 }
-
